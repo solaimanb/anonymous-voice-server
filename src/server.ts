c33e4ae17ee2b6872ca77611539a939dea7f0870
fazlay <rfazlay21@gmail.com>
2024-11-03 01:45:50 +0600

    broadcasting works

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { Server } from 'http';
 import mongoose from 'mongoose';
+import { Server as SocketIOServer } from 'socket.io';
 
 import config from './config/index';
 import { errorlogger, logger } from './shared/logger';
@@ -13,6 +14,7 @@ process.on('uncaughtException', error => {
 
 
 let server: Server;
+let io: SocketIOServer;
 
 
 async function bootstrap() {
@@ -21,11 +23,39 @@ async function bootstrap() {
     // await mongoose.connect("mongodb+srv://crmAdmin:vqtdVpU9nt9XgmJA@cluster0.a4iwm.mongodb.net/?retryWrites=true&w=majority");
     // logger.info(`🛢   Database is connected successfully`);
     console.log(`🛢   Database is connected successfully`);
-
+// Create HTTP server from Express app
     server = app.listen(config.port, () => {
       // logger.info(`Application  listening on port ${config.port}`);
       console.log(`Application  listening on port ${config.port}`);
     });
+    io = new SocketIOServer(server, {
+      cors: {
+        origin: '*', // Update this according to your CORS policy
+        methods: ['GET', 'POST'],
+        credentials: true,
+      },
+    });
+        // Socket.IO connection handling
+        io.on('connection', (socket) => {
+          console.log('User connected:', socket.id);
+          logger.info(`Socket connected: ${socket.id}`);
+    
+          socket.on('message', (data) => {
+            console.log('Message received:', data);
+            io.emit('message', data);
+          });
+    
+          socket.on('disconnect', () => {
+            console.log('User disconnected:', socket.id);
+            logger.info(`Socket disconnected: ${socket.id}`);
+          });
+        });
+    
+        // Start the server
+        server.listen(config.port, () => {
+          console.log(`Application listening on port ${config.port}`);
+        })
+
   } catch (err) {
     errorlogger.error('Failed to connect database', err);
   }
@@ -50,3 +80,5 @@ process.on('SIGTERM', () => {
     server.close();
   }
 });
+// Export io instance for use in other files
+export { io };
