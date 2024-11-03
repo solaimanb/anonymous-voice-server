a975369d6994669283cb7fff3de4704d7e317acc
fazlay <rfazlay21@gmail.com>
2024-11-03 11:36:19 +0600

    socket Initialization

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
 import { Server } from 'http';
 import mongoose from 'mongoose';
-import { Server as SocketIOServer } from 'socket.io';
 
 import config from './config/index';
 import { errorlogger, logger } from './shared/logger';
+import { initializeSocket } from './sockets/socket';
 import app from './app';
 
 process.on('uncaughtException', error => {
@@ -14,7 +14,7 @@ process.on('uncaughtException', error => {
 
 
 let server: Server;
-let io: SocketIOServer;
+
 
 
 async function bootstrap() {
@@ -28,33 +28,10 @@ async function bootstrap() {
       // logger.info(`Application  listening on port ${config.port}`);
       console.log(`Application  listening on port ${config.port}`);
     });
-    io = new SocketIOServer(server, {
-      cors: {
-        origin: '*', // Update this according to your CORS policy
-        methods: ['GET', 'POST'],
-        credentials: true,
-      },
-    });
-        // Socket.IO connection handling
-        io.on('connection', (socket) => {
-          console.log('User connected:', socket.id);
-          logger.info(`Socket connected: ${socket.id}`);
+
+     // Initialize Socket.IO
+     initializeSocket(server);
     
-          socket.on('message', (data) => {
-            console.log('Message received:', data);
-            io.emit('message', data);
-          });
-    
-          socket.on('disconnect', () => {
-            console.log('User disconnected:', socket.id);
-            logger.info(`Socket disconnected: ${socket.id}`);
-          });
-        });
-    
-        // Start the server
-        server.listen(config.port, () => {
-          console.log(`Application listening on port ${config.port}`);
-        })
 
   } catch (err) {
     errorlogger.error('Failed to connect database', err);
@@ -80,5 +57,3 @@ process.on('SIGTERM', () => {
     server.close();
   }
 });
-// Export io instance for use in other files
-export { io };
