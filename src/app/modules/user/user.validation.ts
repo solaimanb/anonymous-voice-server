c57369c659e544b74ed66c15761a892669bc59e7
fazlay <rfazlay21@gmail.com>
2024-11-15 01:12:25 +0600

    refactor: update user validation schema and simplify user details model

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,21 +1,16 @@
 import { z } from "zod";
 
-import { bloodGroup, gender } from "../mentor/mentor.constant";
+import { gender } from "../mentor/mentor.constant";
 
 const createMenteeZodSchema = z.object({
   body: z.object({
     password: z.string().optional(),
+    userName: z.string({
+      required_error: "user name is required",
+    }),
     mentee: z.object({
-      userName: z.string({
-        required_error: "user name is required",
-      }),
       gender: z.enum([...gender] as [string, ...string[]], {
         required_error: "Gender is required",
-      }),
-
-      email: z.string().email().optional(),
-      age: z.string({
-        required_error: "Age is required",
       }),
     }),
   }),
--- src/app/modules/userDetails/userDetails.model.ts
+++ src/app/modules/userDetails/userDetails.model.ts
@@ -1,60 +1,30 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
-import {  IUserDetails, UserDetailsModel } from './userDetails.interface';
+import { IUserDetails, UserDetailsModel } from "./userDetails.interface";
 
 const UserDetailsSchema = new Schema<IUserDetails, UserDetailsModel>(
   {
-   
     name: {
-      type: {
-        firstName: {
-          type: String,
-          // required: true,
-        },
-        lastName: {
-          type: String,
-          // required: true,
-        },
-      },
-    
-    },
-    userName: {
-      type:String,
-      required: true,
-      unique: true,
+      type: String,
     },
     email: {
-      type: String,
-    
-    },
-    dateOfBirth: {
       type: String,
     },
     gender: {
       type: String,
-      enum: ['male', 'female'],
+      enum: ["male", "female"],
       required: true,
     },
-
-    contactNo: {
-      type: String,
-      // unique: true,
-      // required: true,
-    },
-    profileImage: {
-      type: String,
-    }, 
     age: {
       type: Number,
-      required: true,
-    },   
+    },
   },
   {
     timestamps: true,
   }
 );
 
-export const UserDetails = model<IUserDetails, UserDetailsModel>('UserDetails', UserDetailsSchema);
-
-
-
+export const UserDetails = model<IUserDetails, UserDetailsModel>(
+  "UserDetails",
+  UserDetailsSchema
+);
