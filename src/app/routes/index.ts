56c938e825f8db278a0c28e7d722b4e7b077f4e7
fazlay <rfazlay21@gmail.com>
2024-11-25 23:02:31 +0600

    refactor: update route definitions to include appointments and improve formatting

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,50 +1,51 @@
-import express from 'express';
+import express from "express";
 
-import { AuthRoutes } from '../modules/auth/auth.route';
-import { BlogRoutes } from '../modules/blog/blog.route';
-import { FileUploadRoutes } from '../modules/fileUpload/fileUPload.route';
-import { MentorRoutes } from '../modules/mentor/mentor.route';
-import { MessagingRoutes } from '../modules/messaging/messaging.route';
-import { UserRoutes } from '../modules/user/user.route';
-import { UserDetailsRoutes } from '../modules/userDetails/userDetails.route';
+import { AppointmentRoutes } from "../modules/appointment/appointments.route";
+import { AuthRoutes } from "../modules/auth/auth.route";
+import { BlogRoutes } from "../modules/blog/blog.route";
+import { FileUploadRoutes } from "../modules/fileUpload/fileUPload.route";
+import { MentorRoutes } from "../modules/mentor/mentor.route";
+import { MessagingRoutes } from "../modules/messaging/messaging.route";
+import { UserRoutes } from "../modules/user/user.route";
+import { UserDetailsRoutes } from "../modules/userDetails/userDetails.route";
 
 const router = express.Router();
 
 const moduleRoutes = [
-
   {
-    path: '/users',
+    path: "/users",
     route: UserRoutes,
   },
   {
-    path: '/mentors',
+    path: "/mentors",
     route: MentorRoutes,
   },
   {
-    path: '/userDetails',
+    path: "/appointments",
+    route: AppointmentRoutes,
+  },
+  {
+    path: "/userDetails",
     route: UserDetailsRoutes,
   },
   {
-    path: '/auth',
+    path: "/auth",
     route: AuthRoutes,
   },
   {
-    path: '/message',
+    path: "/message",
     route: MessagingRoutes,
   },
 
   {
-    path: '/blog-post',
+    path: "/blog-post",
     route: BlogRoutes,
   },
   {
-    path: '/file-upload',
+    path: "/file-upload",
     route: FileUploadRoutes,
   },
-
-
-
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route));
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 export default router;
